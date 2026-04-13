a4e67ddcad31708bcb4c0a10bb8f571773240ba2
James Lapenna <lapenna.james@yahoo.com>
2026-04-13 17:29:26 -0400

    Update Token.java

--- Token.java
+++ Token.java
@@ -4,6 +4,7 @@ public class Token{
     private final  Type;
     private final String value;
 
+//TODO: Create constructor for Token class(Getter and setters);
 
     public enum Tokens{
         Symbol,
