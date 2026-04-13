3ac7b4aa3452e00bde23df800657e7774b2c90da
James Lapenna <lapenna.james@yahoo.com>
2026-04-13 17:40:36 -0400

    Update Tokenizer.java

--- Tokenizer.java
+++ Tokenizer.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 
 public class Tokenizer extends Token{
 
+    public Tokenizer(String value, Token.Tokens type) {
+        super(value, type);
+    
+    }
     Scanner scan = new Scanner(System.in);
     //initialized Scanner
     int input  = scan.nextInt();
