07677ee658ff2e9ab4b7625f34b482fd64221545
EstSam <143422809+EstSam@users.noreply.github.com>
2026-04-18 19:46:16 -0400

    Update Evaluator.java

--- Evaluator.java
+++ Evaluator.java
@@ -1,1 +1,20 @@
+public class Evaluator {
 
+    public static int evaluate(Node node) {
+        if (node.left == null && node.right == null) {
+            return Integer.parseInt(node.value);
+        }
+
+        int left = evaluate(node.left);
+        int right = evaluate(node.right);
+
+        switch (node.value) {
+            case "+": return left + right;
+            case "-": return left - right;
+            case "*": return left * right;
+            case "/": return left / right;
+        }
+
+        throw new RuntimeException("Invalid operator: " + node.value);
+    }
+}
