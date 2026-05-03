07edd6f9d8d9387fae05352f066ca1791ebb112f
JamesLapenna <JLapenna669@wcupa.edu>
2026-05-03 00:23:46 -0700

    Continuting building Tokenizer

--- Tokenizer.java
+++ Tokenizer.java
@@ -1,23 +1,60 @@
 //Converts input into tokens
 //Breaks the input string into tokens such as numbers, operators, and parentheses.
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Tokenizer extends Token{
+public class Tokenizer{ 
 
-    public Tokenizer(String value, Token.Tokens type) {
-        super(value, type);
-    
+
+    int position = 0;
+    char Char;
+    String input;
+
+
+public Tokenizer(String input) {
+        this.input = input;
     }
-    Scanner scan = new Scanner(System.in);
-    //initialized Scanner
-    int input  = scan.nextInt();
-    //check if int is within input
+
+
+private void Move(){
+    position++;
+    if(position<input.length()){
+        Char = input.charAt(position);
+    }else{
+        Char = '\0';
+    }
+
+}
+
+
+private String Read(){
+    StringBuilder build = new StringBuilder();
+    int count = 0;
+
+    while(Char!='\0'&&Character.isDigit(Char)||Char =='.'){
+        if(Char == '.'){
+            count++;
+        }
+        build.append(Char);
+        Move();
+    }
+    return build.toString();
+}
+
+
+public List<Token> tokenize(){
+        List<Token> tokens = new ArrayList<>();
+    }
+
+
 
     
 
 
 
+if(Char=='+'){
 
+}
 
 }
