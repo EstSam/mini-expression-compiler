3770dbc0713a42175e0a92b218ac3cc8246f35cf
EstSam <143422809+EstSam@users.noreply.github.com>
2026-05-05 12:58:34 -0400

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -1,145 +1,80 @@
-import java.util.List;
+import java.util.*;
 
 public class Parser {
 
     private List<Token> tokens;
-
-    private int position = 0;
+    private int pos = 0;
 
     public Parser(List<Token> tokens) {
-
         this.tokens = tokens;
-
     }
 
     private Token current() {
-
-        if (position < tokens.size()) {
-
-            return tokens.get(position);
-
-        }
-
-        return null;
-
+        return pos < tokens.size() ? tokens.get(pos) : null;
     }
 
     private void eat() {
-
-        position++;
-
+        pos++;
     }
 
-    // E → T ((+ | -) T)*
-
     public Node parseExpression() {
-
         Node node = parseTerm();
 
         while (current() != null &&
-
-               current().getType() == Token.Tokens.OPERATOR &&
-
-               (current().getValue().equals("+") ||
-
-                current().getValue().equals("-"))) {
+               current().getType() == Token.Type.OPERATOR &&
+               (current().getValue().equals("+") || current().getValue().equals("-"))) {
 
             String op = current().getValue();
-
             eat();
 
-            Node newNode = new Node(op);
-
-            newNode.left = node;
-
-            newNode.right = parseTerm();
-
-            node = newNode;
-
+            Node right = parseTerm();
+            node = new Node(op, node, right);
         }
 
         return node;
-
     }
 
-    // T → F ((* | /) F)*
-
     private Node parseTerm() {
-
         Node node = parseFactor();
 
         while (current() != null &&
-
-               current().getType() == Token.Tokens.OPERATOR &&
-
-               (current().getValue().equals("*") ||
-
-                current().getValue().equals("/"))) {
+               current().getType() == Token.Type.OPERATOR &&
+               (current().getValue().equals("*") || current().getValue().equals("/"))) {
 
             String op = current().getValue();
-
             eat();
 
-            Node newNode = new Node(op);
-
-            newNode.left = node;
-
-            newNode.right = parseFactor();
-
-            node = newNode;
-
+            Node right = parseFactor();
+            node = new Node(op, node, right);
         }
 
         return node;
-
     }
 
-    // F → (E) | number
-
     private Node parseFactor() {
-
         Token token = current();
 
         if (token == null) {
-
             throw new RuntimeException("Unexpected end of input");
-
         }
 
-        // NUMBER
-
-        if (token.getType() == Token.Tokens.NUMBER) {
-
+        if (token.getType() == Token.Type.NUMBER) {
             eat();
-
             return new Node(token.getValue());
-
         }
 
-        // (E)
-
-        if (token.getType() == Token.Tokens.LPAREN) {
-
+        if (token.getType() == Token.Type.LPAREN) {
             eat();
-
             Node node = parseExpression();
 
-            if (current() == null ||
-
-                current().getType() != Token.Tokens.RPAREN) {
-
+            if (current() == null || current().getType() != Token.Type.RPAREN) {
                 throw new RuntimeException("Missing closing parenthesis");
-
             }
 
             eat();
-
             return node;
-
         }
 
         throw new RuntimeException("Unexpected token: " + token.getValue());
-
     }
-
 }
